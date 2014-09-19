7925100841c76415380055dec7f57384c2404fa3
radius <radius@radius-v.(none)>
2014-09-20 01:11:18 +0400

    R programming, Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## R programmin, assignment 2
+## Caching the inverse matrix
 
-## Write a short comment describing this function
-
+## This functions implements cached representation of a square invertible matrix. 
+## It receives a square matrix as a paramater.
 makeCacheMatrix <- function(x = matrix()) {
-
+        inverse_matrix <- NULL
+        set <- function(y) {
+                x <<- y
+                inverse_matrix <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(inverse) inverse_matrix <<- inverse
+        getinverse <- function() inverse_matrix
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
-
+## This routine computes the inverse of a matrix with caching the result. It takes as a parameter 
+## a cached representation of a matrix returned by makeCacheMatrix routine defined above
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        inverse <- x$getinverse()
+        if(!is.null(inverse)) {
+                # the inverse of a matrix has already been computed, so just read it from from the cache and return
+                message("getting cached data")
+                return(inverse)
+        }
+        # get the real matrix
+        data <- x$get()
+        # compute inverse by calling solve() routine
+        inverse <- solve(data, ...)
+        # cache the inverse
+        x$setinverse(inverse)
+        inverse
 }
